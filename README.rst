8f48bb8862b47b52c1262298a484a6148f56f41b
EDallas <inmsanz89@gmail.com>
2021-02-08 14:19:31 +0000

    testing

---
+++ README.rst
@@ -0,0 +1,11 @@
+Custom Reports Ama Spine
+=============================================
+
+    This module add custom reports templates for Ama Spine
+
+Credits
+-------
+* Authors
+    * Aresoltec Canarias S.L.
+* Contributors
+    * Inma Sánchez
---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Reports Ama Spine",
+    'summary': """
+        Add custom reports templates for Ama Spine""",
+    'author': "Aresoltec Canarias S.L.",
+    'website': "https://www.aresoltec.com",
+    'category': 'Reports',
+    'version': '0.1',
+    'depends': ['purchase'],
+    'data': [
+        'report/purchase_reports.xml',
+        'report/purchaseorder_document_main.xml',
+        'report/purchaseorder_document_sin_precios_amaspine.xml',
+    ],
+}
